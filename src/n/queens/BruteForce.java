1bdd7312e1e21af9e33a85035dff845e6e6fb5f1
Pasindu Perera <perera.pasindu@gmail.com>
2012-03-26 17:45:29 +0530

    added isSloved method and tollerence to the constructor

--- src/n/queens/BruteForce.java
+++ src/n/queens/BruteForce.java
@@ -4,18 +4,25 @@ package n.queens;
  *
  * @author Rumal
  */
-
 public class BruteForce extends NQueen {
 
-    public BruteForce(int boardSize) {
-        super(boardSize);
+    public BruteForce(int boardSize, int tollerenceCost) {
+        super(boardSize,tollerenceCost);
         currentState = new BruteForceState(boardSize);
     }
 
     @Override
     public void solve() {
         State s = currentState.getNextState();
-        show();
+        while (s != null) {
+            if (isSolvedPossition(s)) {
+                currentState = s;
+                return;
+            }
+            s = currentState.getNextState();
+        }
         
+
     }
+
 }
